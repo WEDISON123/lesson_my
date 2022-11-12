8d65a757d2c1f2357f0823ae54928c39779c1e4f
WEDISON123 <116934315+WEDISON123@users.noreply.github.com>
2022-11-12 16:35:08 +0800

    小程序学习

---
+++ weapp/discovery/pages/index/index.js
@@ -0,0 +1,43 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    title:'布加迪',
+    car:'https://p3.dcarimg.com/img/mosaic-legacy/bef600002ee75271a725~332x0.webp'
+  },
+  // 事件处理函数
+  bindViewTap() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad() {
+    setTimeout(() => {
+      title:'aaa';
+      car:'https://p3.dcarimg.com/img/mosaic-legacy/bef500005c1ff4a095cd~332x0.webp'
+    }, 3000);
+  },
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    })
+  },
+  getUserInfo(e) {
+    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
+    console.log(e)
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
